10fed43d87031061f41f37f812d8992889c45cff
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-09-07 18:17:07 +0530

    feat: add RelatedDoctors component to display a list of related doctors based on specialty
    - Displays up to 5 related doctors, excluding the currently viewed doctor
    - Shows doctor image, name, availability, and specialty
    - Allows users to navigate to doctor details and book an appointment

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
+import RelatedDoctors from "../components/RelatedDoctors";
 
 const Appointment = () => {
   const { docId } = useParams();
@@ -168,6 +169,9 @@ const Appointment = () => {
             Book an appointment
           </button>
         </div>
+
+        {/* ---------------Listing related doctors--------------- */}
+        <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
       </div>
     )
   );
